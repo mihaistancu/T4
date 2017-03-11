24b963c714906c4c8dbc64cbf6dc27551cd27f61
alekwtz <alekwtzz@gmail.com>
2017-03-11 19:25:13 +0200

    Added IntentService

---
+++ speech/T4.Business/Application/IntentService.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Cognitive.LUIS;
+
+namespace T4.Business.Application
+{
+    public static class IntentService
+    {
+        public static LuisResult GetIntent(string text)
+        {
+            var client = new LuisClient("7c263174-cc8c-4107-a4de-55b1e9aa8bce", "0888ecfdb8404a7ab998888aeeb6f8ec");
+            return client.Predict(text).Result;
+        }
+    }
+}

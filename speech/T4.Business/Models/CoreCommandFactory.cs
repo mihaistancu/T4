8eb4102d1a37a9c58c0f22a87f0b010107de22da
andreimpircalaboiu <andrei.pircalaboiu@gmail.com>
2017-03-11 17:33:22 +0200

    added business project to solution

---
+++ speech/T4.Business/Models/CoreCommandFactory.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using T4.Business.Models.Commands;
+using T4.Business.Models.Interfaces;
+
+namespace T4.Business.Models
+{
+    public class CoreCommandFactory : ICoreCommandFactory
+    {
+        public ICoreCommand Create(string intent)
+        {
+            switch (intent)
+            {
+                case "translation":
+                {
+                    return new TranslateCommand();
+                }
+                case "notes":
+                {
+                    return new NotesCommand();
+                }
+                default: throw new NotImplementedException();
+            }
+            
+        }
+    }
+}

030331feb65f054c79ec8f3084c823b95f5bdcfa
andreimpircalaboiu <andrei.pircalaboiu@gmail.com>
2017-03-11 19:32:16 +0200

    added command repo + custom command interface

---
+++ speech/T4.Business/Application/CommandRepository.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using T4.Business.Models.Interfaces;
+
+namespace T4.Business.Application
+{
+    public class CommandRepository
+    {
+        private IList<ICustomCommand> _savedCommands { get; set; }
+
+        public bool HasCommand(string text)
+        {
+       
+            return _savedCommands.Any(i=>i.GetIntent() == text);
+        }
+        public ICustomCommand GetCommand(string text)
+        {
+            return _savedCommands.FirstOrDefault(i => i.GetText() == text);
+        }
+    }
+}

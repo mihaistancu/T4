d3a7febdb95a3abd7cdc89ca88400ce2211fa272
alekwtz <alekwtzz@gmail.com>
2017-03-11 17:04:40 +0200

    Added Google Speech API solution

---
+++ speech/Recognize/Recognize.cs
@@ -0,0 +1,282 @@
+﻿/*
+ * Copyright (c) 2017 Google Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
+using CommandLine;
+using Google.Cloud.Speech.V1Beta1;
+using System;
+using System.Diagnostics;
+using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace GoogleCloudSamples
+{
+    class Options
+    {
+        [Value(0, HelpText = "A path to a sound file.  Encoding must be "
+            + "Linear16 with a sample rate of 16000.", Required = true)]
+        public string FilePath { get; set; }
+    }
+
+    [Verb("sync", HelpText = "Detects speech in an audio file.")]
+    class SyncOptions : Options { }
+
+    [Verb("async", HelpText = "Creates a job to detect speech in an audio "
+        + "file, and waits for the job to complete.")]
+    class AsyncOptions : Options { }
+
+    [Verb("stream", HelpText = "Detects speech in an audio file by streaming "
+        + "it to the Speech API.")]
+    class StreamingOptions : Options { }
+
+    [Verb("listen", HelpText = "Detects speech in a microphone input stream.")]
+    class ListenOptions
+    {
+        [Value(0, HelpText = "Number of seconds to listen for.", Required = false)]
+        public int Seconds { get; set; } = int.MaxValue;
+    }
+
+    [Verb("rec", HelpText = "Detects speech in an audio file. Supports other file formats.")]
+    class RecOptions : Options
+    {
+        [Option('b', Default = 16000, HelpText = "Sample rate in bits per second.")]
+        public int BitRate { get; set; }
+
+        [Option('e', Default = RecognitionConfig.Types.AudioEncoding.Linear16,
+            HelpText = "Audio file encoding format.")]
+        public RecognitionConfig.Types.AudioEncoding Encoding { get; set; }
+    }
+
+
+    public class Recognize
+    {
+        static object Rec(string filePath, int bitRate,
+            RecognitionConfig.Types.AudioEncoding encoding)
+        {
+            var speech = SpeechClient.Create();
+            var response = speech.SyncRecognize(new RecognitionConfig()
+            {
+                Encoding = encoding,
+                SampleRate = bitRate,
+            }, RecognitionAudio.FromFile(filePath));
+            foreach (var result in response.Results)
+            {
+                foreach (var alternative in result.Alternatives)
+                {
+                    Console.WriteLine(alternative.Transcript);
+                }
+            }
+            return 0;
+        }
+
+        // [START speech_sync_recognize]
+        static object SyncRecognize(string filePath)
+        {
+            var speech = SpeechClient.Create();
+            var response = speech.SyncRecognize(new RecognitionConfig()
+            {
+                Encoding = RecognitionConfig.Types.AudioEncoding.Linear16,
+                SampleRate = 16000,
+            }, RecognitionAudio.FromFile(filePath));
+            foreach (var result in response.Results)
+            {
+                foreach (var alternative in result.Alternatives)
+                {
+                    Console.WriteLine(alternative.Transcript);
+                }
+            }
+            return 0;
+        }
+        // [END speech_sync_recognize]
+
+        // [START speech_async_recognize]
+        static object AsyncRecognize(string filePath)
+        {
+            var speech = SpeechClient.Create();
+            var longOperation = speech.AsyncRecognize(new RecognitionConfig()
+            {
+                Encoding = RecognitionConfig.Types.AudioEncoding.Linear16,
+                SampleRate = 16000,
+            }, RecognitionAudio.FromFile(filePath));
+            longOperation = longOperation.PollUntilCompleted();
+            var response = longOperation.Result;
+            foreach (var result in response.Results)
+            {
+                foreach (var alternative in result.Alternatives)
+                {
+                    Console.WriteLine(alternative.Transcript);
+                }
+            }
+            return 0;
+        }
+        // [END speech_async_recognize]
+
+        /// <summary>
+        /// Stream the content of the file to the API in 32kb chunks.
+        /// </summary>
+        // [START speech_streaming_recognize]
+        static async Task<object> StreamingRecognizeAsync(string filePath)
+        {
+            var speech = SpeechClient.Create();
+            var streamingCall = speech.GrpcClient.StreamingRecognize();
+            // Write the initial request with the config.
+            await streamingCall.RequestStream.WriteAsync(
+                new StreamingRecognizeRequest()
+                {
+                    StreamingConfig = new StreamingRecognitionConfig()
+                    {
+                        Config = new RecognitionConfig()
+                        {
+                            Encoding =
+                            RecognitionConfig.Types.AudioEncoding.Linear16,
+                            SampleRate = 16000,
+                        },
+                        InterimResults = true,
+                    }
+                });
+            // Print responses as they arrive.
+            Task printResponses = Task.Run(async () =>
+            {
+                while (await streamingCall.ResponseStream.MoveNext(
+                    default(CancellationToken)))
+                {
+                    foreach (var result in streamingCall.ResponseStream
+                        .Current.Results)
+                    {
+                        foreach (var alternative in result.Alternatives)
+                        {
+                            Console.WriteLine(alternative.Transcript);
+                        }
+                    }
+                }
+            });
+            // Stream the file content to the API.  Write 2 32kb chunks per 
+            // second.
+            using (FileStream fileStream = new FileStream(filePath, FileMode.Open))
+            {
+                var buffer = new byte[32 * 1024];
+                int bytesRead;
+                while ((bytesRead = await fileStream.ReadAsync(
+                    buffer, 0, buffer.Length)) > 0)
+                {
+                    await streamingCall.RequestStream.WriteAsync(
+                        new StreamingRecognizeRequest()
+                        {
+                            AudioContent = Google.Protobuf.ByteString
+                            .CopyFrom(buffer, 0, bytesRead),
+                        });
+                    await Task.Delay(500);
+                };
+            }
+            await streamingCall.RequestStream.CompleteAsync();
+            await printResponses;
+            return 0;
+        }
+        // [END speech_streaming_recognize]
+
+        // [START speech_streaming_mic_recognize]
+        static async Task<object> StreamingMicRecognizeAsync(int seconds)
+        {
+            if (NAudio.Wave.WaveIn.DeviceCount < 1)
+            {
+                Console.WriteLine("No microphone!");
+                return -1;
+            }
+            var speech = SpeechClient.Create();
+            var streamingCall = speech.GrpcClient.StreamingRecognize();
+            // Write the initial request with the config.
+            await streamingCall.RequestStream.WriteAsync(
+                new StreamingRecognizeRequest()
+                {
+                    StreamingConfig = new StreamingRecognitionConfig()
+                    {
+                        Config = new RecognitionConfig()
+                        {
+                            Encoding =
+                            RecognitionConfig.Types.AudioEncoding.Linear16,
+                            SampleRate = 16000,
+                        },
+                        InterimResults = true,
+                    }
+                });
+            // Print responses as they arrive.
+            Task printResponses = Task.Run(async () =>
+            {
+                while (await streamingCall.ResponseStream.MoveNext(
+                    default(CancellationToken)))
+                {
+                    foreach (var result in streamingCall.ResponseStream
+                        .Current.Results)
+                    {
+                        foreach (var alternative in result.Alternatives)
+                        {
+                            Console.WriteLine(alternative.Transcript);
+                        }
+                    }
+                }
+            });
+            // Read from the microphone and stream to API.
+            object writeLock = new object();
+            bool writeMore = true;
+            var waveIn = new NAudio.Wave.WaveInEvent();
+            waveIn.DeviceNumber = 0;
+            waveIn.WaveFormat = new NAudio.Wave.WaveFormat(16000, 1);
+            waveIn.DataAvailable +=
+                (object sender, NAudio.Wave.WaveInEventArgs args) =>
+                {
+                    lock (writeLock)
+                    {
+                        if (!writeMore) return;
+                        streamingCall.RequestStream.WriteAsync(
+                            new StreamingRecognizeRequest()
+                            {
+                                AudioContent = Google.Protobuf.ByteString
+                                    .CopyFrom(args.Buffer, 0, args.BytesRecorded)
+                            }).Wait();
+                    }
+                };
+            waveIn.StartRecording();
+            Console.WriteLine("Speak now.");
+            await Task.Delay(TimeSpan.FromSeconds(seconds));
+            // Stop recording and shut down.
+            waveIn.StopRecording();
+            lock (writeLock) writeMore = false;
+            await streamingCall.RequestStream.CompleteAsync();
+            await printResponses;
+            return 0;
+        }
+        // [END speech_streaming_mic_recognize]
+
+        public static int Main(string[] args)
+        {
+            return (int)Parser.Default.ParseArguments<
+                SyncOptions, 
+                AsyncOptions,
+                StreamingOptions, 
+                ListenOptions,
+                RecOptions>(args)
+                
+                
+                .MapResult(
+                (SyncOptions opts) => SyncRecognize(opts.FilePath),
+                (AsyncOptions opts) => AsyncRecognize(opts.FilePath),
+                (StreamingOptions opts) => StreamingRecognizeAsync(opts.FilePath).Result,
+                (ListenOptions opts) => StreamingMicRecognizeAsync(100).Result,
+                (RecOptions opts) => Rec(opts.FilePath, opts.BitRate, opts.Encoding),
+                errs => 1);
+        }
+    }
+}
